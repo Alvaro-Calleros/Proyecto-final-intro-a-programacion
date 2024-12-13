5796bc3709c66116b26692bb4ff053fcdda29d1a
Alvaro-Calleros <alvarocalleros44@gmail.com>
2024-12-12 18:27:26 -0700

    Edicon sencilla del archivo de efectos con JavaScript, se agrego un cookie-footer y aqui esta el efecto para que desaparezca cuando se da click en aceptar

--- js/efects.js
+++ js/efects.js
@@ -1,13 +1,17 @@
+document.getElementById('cookie-accept').addEventListener('click', function() {
+    document.getElementById('cookie-footer').style.display = 'none';
+});
+
+
 const links = document.querySelectorAll('nav a[href^="#"]');
 
 links.forEach(link => {
     link.addEventListener('click', function(event) {
-
         event.preventDefault();
-
+        
         const targetId = this.getAttribute('href');
         const targetSection = document.querySelector(targetId);
-
+        
         if (targetSection) {
             targetSection.scrollIntoView({
                 behavior: 'smooth',
